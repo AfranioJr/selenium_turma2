176238b15750474cb5aea0c4895b0d519e3265c0
afraniosjr <idd_asouza@uolinc.com>
2021-06-29 23:00:21 -0300

    Solução do Exercicio 01 Selenium

---
+++ src/test/java/selenium_turma2/PrimeiraClasseSelenium.java
@@ -0,0 +1,20 @@
+package selenium_turma2;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PrimeiraClasseSelenium {
+    @Test
+    public void testelogin(){
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://demo.automationtesting.in/Register.html");
+        driver.findElement(By.xpath("//*[@id='basicBootstrapForm']/div[1]/div[1]/input")).sendKeys("Afrânio");
+        driver.findElement(By.xpath("//*[@id='basicBootstrapForm']/div[1]/div[2]/input")).sendKeys("Junior");
+        
+        driver.findElement(By.xpath("//*[@id='basicBootstrapForm']/div[2]/div/textarea")).sendKeys("Rua Dona Brigida, 597");
+        driver.findElement(By.xpath("//*[@id='eid']/input")).sendKeys("emaildetestes@teste.com");
+        driver.findElement(By.xpath("//*[@id='basicBootstrapForm']/div[4]/div/input")).sendKeys("(11) 98765-4321");
+        
+    }
+}
